8d4d99e1cb50f553f554a9c5fa7af0ec9fcb2941
rahilwisdom <mister.r432000@gmail.com>
2021-05-26 06:44:05 +0800

    Test

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 scaler = MinMaxScaler(feature_range=(0,1))
-model = load_model('my_model.h5')
-df = pd.read_csv('commodity_trade_statistics_data.csv')
+
 
 
 @app.route('/')
@@ -17,6 +16,8 @@ def home():
 
 @app.route('/predict_api', methods=['POST'])
 def predict_():
+    model = load_model('my_model.h5')
+    df = pd.read_csv('commodity_trade_statistics_data.csv')
     #commodity = str(request.form['commodity'])
     #country = str(request.form['country'])
     json_ = request.json
